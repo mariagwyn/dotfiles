f2822c0634f1b189013bad22093ae5a7c50fb7ad
Maria McDowell <mariagwyn@gmail.com>
2020-04-06 08:59:01 -0700

    Removing brew php71 and python 2.7 links.

--- bash/path.sh
+++ bash/path.sh
@@ -1,5 +1,4 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTS/bin:$PATH"
 
-export PATH="$(brew --prefix homebrew/php/php71)/bin:$PATH"
-
-export PATH="/Users/maria.mcdowell/Library/Python/2.7/bin/:$PATH"
+export PATH="/usr/local/opt/php@7.3/bin:$PATH"
+export PATH="/usr/local/opt/php@7.3/sbin:$PATH"
